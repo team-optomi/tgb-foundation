7342182766097cbd77630c46ed10161a70751624
theremotecreative <jason@theremotecreative.com>
2022-01-13 15:46:16 -0600

    build time troubleshooting

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from 'styled-components'
 
-import Layout from "../components/layout"
+import Layout from "../components/layout-v2"
 import Seo from "../components/seo"
 
 const NotFoundPage = ({ data, location }) => {
@@ -10,11 +11,32 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <MainSection>
+        <div>
+          <h1>404: Not Found</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </div>
+      </MainSection>
     </Layout>
   )
 }
+
+const MainSection = styled.section`
+  min-height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    font-family: "Bodoni Classic";
+    font-size: 36px;
+    text-align: center;
+  }
+  p {
+    font-family: "Bodoni Classic";
+    font-size: 24px;
+    text-align: center;
+  }
+`
 
 export default NotFoundPage
 
